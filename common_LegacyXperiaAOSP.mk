f51ff9de76e52483cfb388f9d70c533520988283
Michał Gapiński <mike@gapinski.tk>
2015-03-04 23:33:51 +0100

    build stagefright FFMPEG plugin and some tools

--- common_LegacyXperiaAOSP.mk
+++ common_LegacyXperiaAOSP.mk
@@ -3,11 +3,22 @@
 # Version
 LX_VERSION_NUMBER := alpha1
 
-# NOPE Unofficial packages that I used from trees outside AOSP
-#PRODUCT_PACKAGES += \
-#    Superuser \
-#    su 
-	
+# Stagefright FFMPEG plugin
+PRODUCT_PACKAGES += \
+    libstagefright_soft_ffmpegadec \
+    libstagefright_soft_ffmpegvdec \
+    libFFmpegExtractor \
+    libnamparser
+    
+# Extra tools 
+PRODUCT_PACKAGES += \
+    e2fsck \
+    mke2fs \
+    tune2fs \
+    mount.exfat \
+    fsck.exfat \
+    mkfs.exfat 
+
 PRODUCT_COPY_FILES += \
 	vendor/lx/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
 
