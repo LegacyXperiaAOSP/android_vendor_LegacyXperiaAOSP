de90c3c2016ce08ae815e029b35e9ec92f8a2eba
Michał Gapiński <mikegapinski@users.noreply.github.com>
2015-03-15 12:11:31 +0100

    Update AndroidProducts.mk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,11 +1,13 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/LegacyXperiaAOSP_anzu.mk  \
-    $(LOCAL_DIR)/LegacyXperiaAOSP_coconut.mk  \
-    $(LOCAL_DIR)/LegacyXperiaAOSP_haida.mk  \
     $(LOCAL_DIR)/LegacyXperiaAOSP_hallon.mk  \
-    $(LOCAL_DIR)/LegacyXperiaAOSP_iyokan.mk   \ 
+    $(LOCAL_DIR)/LegacyXperiaAOSP_iyokan.mk   \
     $(LOCAL_DIR)/LegacyXperiaAOSP_mango.mk   \
     $(LOCAL_DIR)/LegacyXperiaAOSP_phoenix.mk  \
     $(LOCAL_DIR)/LegacyXperiaAOSP_satsuma.mk   \
     $(LOCAL_DIR)/LegacyXperiaAOSP_smultron.mk  \
-    $(LOCAL_DIR)/LegacyXperiaAOSP_urushi.mk                    
+    $(LOCAL_DIR)/LegacyXperiaAOSP_urushi.mk     \
+    $(LOCAL_DIR)/LegacyXperiaAOSP_anzu.mk  \
+    $(LOCAL_DIR)/LegacyXperiaAOSP_coconut.mk  \
+    $(LOCAL_DIR)/LegacyXperiaAOSP_haida.mk  \
+    $(LOCAL_DIR)/LegacyXperiaAOSP_zeus.mk
+
