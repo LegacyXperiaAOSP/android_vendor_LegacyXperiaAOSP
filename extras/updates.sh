163d7417e452f7bb048cf1e0a13297bacdd46a1a
Michał Gapiński <mikegapinski@users.noreply.github.com>
2015-03-19 18:18:07 +0100

    Update updates.sh

--- extras/updates.sh
+++ extras/updates.sh
@@ -21,15 +21,6 @@ cherries+=(CM_88635)
 # Import updated yaffs2 driver
 cherries+=(LX_13)
 
-# Revert "msm7x30: Set max background services to 4"
-cherries+=(LX_213)
-
-# Remove performance profiles, they are not supported on AOSP
-cherries+=(LX_232)
-
-# Add config_sf_limitedAlpha to overlay
-cherries+=(LX_231)
-
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
 else
