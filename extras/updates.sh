956d36132163fe3e91872af7d1ed74e58a4f2d80
Michael Bestas <mikeioannina@gmail.com>
2015-04-24 01:50:02 +0300

    Update cherries

--- extras/updates.sh
+++ extras/updates.sh
@@ -10,15 +10,6 @@ android="${PWD}"
 # Add LegacyXperiaAOSP DeviceSettings
 cherries+=(LX_252)
 
-# msm7x30-common: Enable BOARD_EGL_WORKAROUND_BUG_10194508 flag
-cherries+=(LX_259)
-
-# audio: Make getPresentationPosition pure virtual
-cherries+=(CM_92381)
-
-# fix AudioPolicyManager assert when making a call
-cherries+=(LX_296)
-
 # Add local cherries if they exist
 if [ -f ${android}/updates-local.sh ]; then
     source ${android}/updates-local.sh
